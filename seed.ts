4d68ce587e82ec8df0f45a3c6f9c86523557bd0c
Radomir Radojevic <radomir.radojevic@gmail.com>
2026-04-20 23:20:04 +0200

    feat(seed): add initial seed script to populate database with sample data

---
+++ seed.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+import { neon } from '@neondatabase/serverless';
+
+async function main() {
+  const sql = neon(process.env.DATABASE_URL!);
+
+  const result = await sql`SELECT id, short_code, original_url FROM links WHERE user_id = 'user_3CXq92cuWjXPb4BJtCiZmc14f31'`;
+  console.table(result);
+}
+
+main().catch(console.error);
