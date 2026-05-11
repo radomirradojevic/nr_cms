dcdefdabf20362849333c8de6c67995e8d5e35c0
Radomir Radojevic <radomir.radojevic@gmail.com>
2026-05-12 01:17:22 +0200

    feat: refactor database connection logic and add PostgreSQL support

--- db/index.ts
+++ db/index.ts
@@ -1,4 +1,9 @@
-import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle as drizzleNeon } from "drizzle-orm/neon-http";
+import { drizzle as drizzlePg } from "drizzle-orm/node-postgres";
 
-const db = drizzle(process.env.DATABASE_URL!);
+const url = process.env.DATABASE_URL!;
+const isNeon = url.includes(".neon.tech");
+
+const db = isNeon ? drizzleNeon(url) : drizzlePg(url);
+
 export { db };
